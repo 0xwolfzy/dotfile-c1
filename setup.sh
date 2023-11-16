4e5afc405a9103f391569eae5fd8a0730853e074
0xwolfzy <130280669+0xwolfzy@users.noreply.github.com>
2023-11-16 17:50:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 !#/bin/sh
-apt install xorg light slick-greeter lightdm-settings lxappearance nitrogen pcmanfm pulseaudio alsa-utils pavucontrol kitty firefox -y
+apt install xorg light slick-greeter lightdm-settings lxappearance nitrogen pcmanfm pulseaudio alsa-utils pavucontrol kitty -y
+
+# ---------------------- install firefox -------------------------------------------------------------
+wget -O firefoxsetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+sudo tar -xf firefoxsetup.tar.bz2 --directory /opt
