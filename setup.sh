ee4b2f9c2af17c9dacd4d76bfbdc3c89540492f2
0xwolfzy <130280669+0xwolfzy@users.noreply.github.com>
2023-11-30 17:15:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 !#/bin/sh
 apt install xorg light slick-greeter lightdm-settings lxappearance nitrogen pcmanfm pulseaudio alsa-utils pavucontrol kitty -y
 
-# ---------------------- install firefox -------------------------------------------------------------
+# ---------------------- install firefox ----------------------------------------------------------------------
 wget -O firefoxsetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 sudo tar -xf firefoxsetup.tar.bz2 --directory /opt
 
-# ------------------------- install polybar ---------------------------------------------------------
-sudo apt install polybar
+# ------------------------- install polybar -------------------------------------------------------------------
+sudo apt install polybar -y
+
+#------------------------- install alacritty ------------------------------------------------------------------
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+cargo install alacritty
